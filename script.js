f201469f664677281c44886ae1d6d09bcd236dd8
Anushka Tamrakar <78794131+Anushka02tamrakar@users.noreply.github.com>
2024-01-06 12:04:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let buttons = document.querySelectorAll('.keyboard input[type="button"]');
+let input = document.getElementById('input');
+
+let string = "";
+
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        
+        if(e.target.value == '='){
+            string = eval(string);
+            input.value = string;
+            evaluated = true;
+        
+        }
+        else if(e.target.value == 'RESET'){
+            string = "";
+            input.value = string;
+        }
+    
+        else if(e.target.value == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.value;
+            input.value = string;
+        }
+    })
+})
